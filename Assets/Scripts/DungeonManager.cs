7a8c08a92a46e40af5e6a3f318fd9f4e17d49ad7
donsvederus <svederus@gmail.com>
2021-01-23 21:56:09 -0700

    0.10 Random Walker and Exit Doorway
    Random Dungeon Generation

--- Assets/Scripts/DungeonManager.cs
+++ Assets/Scripts/DungeonManager.cs
@@ -1,12 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DungeonManager : MonoBehaviour
 {
 
-    public GameObject floorPrefab, wallPrefab;
+    public GameObject floorPrefab, wallPrefab, tilePrefab, exitPrefab;
+    public int totalFloorCount; 
 
     [HideInInspector] public float minX, maxX, minY, maxY;
+
+    List<Vector3> floorList = new List<Vector3>();
+    
+    void Start() {
+        RandomWalker();
+    }
+
+    void Update() {
+        if(Application.isEditor && Input.GetKeyDown(KeyCode.Backspace)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void RandomWalker() {
+        Vector3 curPos = Vector3.zero;
+        floorList.Add(curPos);
+
+        while(floorList.Count < totalFloorCount) {
+            switch(Random.Range(1, 5)) {
+                case 1: curPos += Vector3.up;       break;
+                case 2: curPos += Vector3.right;    break;
+                case 3: curPos += Vector3.down;     break;
+                case 4: curPos += Vector3.left;     break;
+            }
+        
+
+            bool inFloorList = false;
+            for (int i = 0; i < floorList.Count;  i++ ) {
+                if(Vector3.Equals(curPos, floorList[i])) {
+                    inFloorList = true;
+                    break;
+                }
+            }
+
+            if(!inFloorList) {
+                floorList.Add(curPos);
+            }
+        }
+
+        for (int i = 0; i < floorList.Count; i++ ) {
+            GameObject goTile = Instantiate(tilePrefab, floorList[i], Quaternion.identity) as GameObject;
+            goTile.name = tilePrefab.name;
+            goTile.transform.SetParent(transform);
+        }
+
+        StartCoroutine(DelayProgress());
+    }
+
+    IEnumerator DelayProgress() {
+        while(FindObjectsOfType<TileSpawner>().Length > 0) {
+            yield return null;
+        }
+        ExitDoorway();
+    }
+
+    void ExitDoorway() {
+        Vector3 doorPos = floorList[floorList.Count - 1];
+        GameObject goDoor = Instantiate(exitPrefab, doorPos, Quaternion.identity) as GameObject;
+        goDoor.name = exitPrefab.name;
+        goDoor.transform.SetParent(transform);
+    }
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,76 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour 
-{
+public class Player : MonoBehaviour {
     public float speed;
 
-    LayerMask obstacleMask;  // assigning layer masks to variable
-    Vector2 targetPosition;  // this is where we want to move too  
-    Transform playerSprite;  // transform assigned to playerSprite
-    float flipX;  // assign float flipX
-    bool isMoving;  // assigning false by default to isMoving variable
+    LayerMask obstacleMask; 
+    Vector2 targetPosition;  
+    Transform playerSprite;  
+    float flipX;  
+    bool isMoving;  
 
-    void Start() // This happens at the start
-    {
-        //
+    void Start() {
         obstacleMask = LayerMask.GetMask("Wall", "Enemy");
-        // Get the transform properties
         playerSprite = GetComponentInChildren<SpriteRenderer>().transform;
-        // Assigns X transform properties to flipX, which is 1
-        // Note:  make sure sprite is facing right to begin, Fip the X in Unity
         flipX = playerSprite.localScale.x;
     }
 
-    void Update() // This happens every frame after start
-    {
-        // Gets horizontal input, without decimals, so -1 for left, or 1 for right, then Assigns it to horizaontalMovement 
+    void Update() {
+        Move();
+    }
+
+    void Move() {
         float horizontalMovement = System.Math.Sign(Input.GetAxisRaw("Horizontal"));
-        float verticalMovement = System.Math.Sign(Input.GetAxisRaw("Vertical")); // Get Vertical input
-        // Debug.Log("Horizontal: " + horizontalMovement);
+        float verticalMovement = System.Math.Sign(Input.GetAxisRaw("Vertical"));
 
-        // This makes sure the character is moving in any of the 4 directions
-        if(Mathf.Abs(horizontalMovement) > 0 || Mathf.Abs(verticalMovement) > 0) {
 
-            // Flipping the character direction
-            // If the horizontalMovement is greater then 0
-            if(Mathf.Abs(horizontalMovement) > 0) 
+        if (Mathf.Abs(horizontalMovement) > 0 || Mathf.Abs(verticalMovement) > 0)
+        {
+            if (Mathf.Abs(horizontalMovement) > 0)
             {
-                // This changes the x-transform properties by multiplying flipX with horizontalMovement
-                //  1 flipX x 1 horizontalMovement = 1 on the x-transform 
-                //  1 flipX x -1 horizontalMovement = -1 on the x-transform 
                 playerSprite.localScale = new Vector2(flipX * horizontalMovement, playerSprite.localScale.y);
-                // Debug.Log("Horizontal: " + horizontalMovement);
-                // Debug.Log("flipX: " + flipX);
             }
-            
-            // Check to see ifMoving
-            if(!isMoving) {
 
-                // Character Movement
-                if(Mathf.Abs(horizontalMovement) > 0) 
+            // Check to see ifMoving
+            if (!isMoving)
+            {
+                if (Mathf.Abs(horizontalMovement) > 0)
                 {
-                    // horizontal movement
                     targetPosition = new Vector2(transform.position.x + horizontalMovement, transform.position.y);
                 }
-                else if(Mathf.Abs(verticalMovement) > 0) 
+                else if (Mathf.Abs(verticalMovement) > 0)
                 {
-                    // vertical movement
                     targetPosition = new Vector2(transform.position.x, transform.position.y + verticalMovement);
                 }
 
-                // check to see if player collides into something
-                // assigning the size of the hit box
                 Vector2 hitSize = Vector2.one * 0.8f;
                 Collider2D hitSomething = Physics2D.OverlapBox(
-                    targetPosition, 
-                    hitSize, 
-                    0, 
+                    targetPosition,
+                    hitSize,
+                    0,
                     obstacleMask
                 );
 
-                // Check to see if player hit anything, if not! then start move coroutine
-                if(!hitSomething) 
+                if (!hitSomething)
                 {
                     StartCoroutine(GoodMove());
                 }
